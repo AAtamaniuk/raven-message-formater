5d82e7c7b315c178014785358b32b3fcb42e6966
Oleksandr Atamaniuk <aatamaniuk.dev@gmail.com>
2023-06-28 14:33:59 +0300

    add message form

---
+++ src/MessageForm.jsx
@@ -0,0 +1,78 @@
+import React from 'react'
+import PropTypes from 'prop-types'
+import {
+    FormControl,
+    FormLabel,
+    Input,
+    NumberInput,
+    NumberInputField,
+    Textarea,
+    Card,
+    CardBody,
+    Text,
+    Button,
+} from '@chakra-ui/react'
+
+const MessageForm = () => {
+    const [frequency, setFrequency] = React.useState('')
+    const [time, setTime] = React.useState('')
+    const [unit, setUnit] = React.useState('нв підрозділ')
+
+    const handleFraquncyChange = (event) => setFrequency(event.target.value)
+    const handleTimeChange = (event) => setTime(event.target.value)
+    const handleUnitChange = (event) => setUnit(event.target.value)
+
+    const coordinateX = '48.4993';
+    const coordinateY = '38.7867';
+
+
+    const formatedMessage = `${frequency}/${time}\n${unit}\nX:${coordinateX}\nY:${coordinateY}`
+
+    const copyContent = async () => {
+    try {
+      await navigator.clipboard.writeText(formatedMessage);
+      console.log('Content copied to clipboard');
+    } catch (err) {
+      console.error('Failed to copy: ', err);
+    }
+  }
+
+    return (
+        <div>
+            <NumberInput precision={3}>
+                <FormLabel>Частота</FormLabel>
+                <NumberInputField onChange={handleFraquncyChange} value={frequency} />
+            </NumberInput>
+
+            <FormControl>
+                <FormLabel>Час</FormLabel>
+                <Input type='time' onChange={handleTimeChange} value={time} />
+            </FormControl>
+
+            <FormControl>
+                <FormLabel>Назва підрозділу</FormLabel>
+                <Input onChange={setUnit} value={unit} />
+            </FormControl>
+
+            <FormControl mb={15}>
+                <FormLabel>Координати</FormLabel>
+                <Textarea />
+            </FormControl>
+
+            <Card mb={15}>
+                <CardBody>
+                    <Text>{`${frequency}/${time}`}</Text>
+                    <Text>{`${unit}`}</Text>
+                    <Text>{`X:${coordinateX}`}</Text>
+                    <Text>{`Y:${coordinateY}`}</Text>
+                </CardBody>
+            </Card>
+
+            <Button onClick={copyContent}>Скопіювати</Button>
+        </div>
+    )
+}
+
+MessageForm.propTypes = {}
+
+export default MessageForm
